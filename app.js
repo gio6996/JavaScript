a1231bd3275c603b2b37fc1edf9d3a6aad56b384
gio6996 <75396061+gio6996@users.noreply.github.com>
2023-03-07 10:11:17 -0600

    Create app.js
    app calculadora

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('Aplicacion Calculadora');
+
+function sumar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    document.getElementById('resultado').innerHTML = resultado;
+}
